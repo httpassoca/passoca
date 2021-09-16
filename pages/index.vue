f3e8f3a55b4e6d13c3864c253809983c005d2d8a
rafonel <rfreitas@soujmv.com>
2021-09-16 13:25:09 -0300

    hm

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,12 @@ Content(classe="flex-col md:flex-row items-center justify-center md:justify-betw
     h6 Hi, I'm
     h1.font-boston Rafael Passoca
     h2.font-boston And I code.
-    p(v-html="info")
+    p As a Frontend Engineer, I believe that appearence and praticy are the best influences to the user. 
+    p For me, <i>it is that matters. And I love to make it happen.</i>
+    p Currently, I'm working at <b> JMV Technology </b> with Nochalks, a distance education project. 
   img.profile-pic(src="https://avatars.githubusercontent.com/u/49541181?v=4" alt="me")
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      info: `
-          As a Frontend Engineer, I believe that appearence and praticy are the best influences to the user. For me,
-    <i> it is that matters. </i>
-    Currently, I'm working at
-    <b> JMV Technology </b>
-    , with
-    <b> Nochalks </b>
-    , a distance education project.
-    `
-    }
-  }
-}
-</script>
 <style lang="sass" scoped>
 .profile-pic
   @apply rounded-full m-4
