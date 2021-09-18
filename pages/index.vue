e5f1c9814e22d625eb7ce47920c0c9eb5d14698e
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2021-09-18 12:59:51 -0300

    trying to make a beautiful link

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@ Content(classe="flex-col md:flex-row items-center justify-center md:justify-betw
     h1.font-boston Rafael Passoca
     h2.font-boston And I code.
     p As a Frontend Engineer, I believe that appearence and praticy are the best influences to the user. 
-    p For me, <i>it is that matters. And I love to make it happen.</i>
-    p Currently, I'm working at <b> JMV Technology </b> with Nochalks, a distance education project. 
+    p For me, <b>it is that matters. And I love to make it happen.</b>
+    p Currently, I'm working at <a href="https://jmvtechnology.com/"> JMV Technology </a> with <b> Nochalks</b>, a distance education project. 
   img.profile-pic(src="https://avatars.githubusercontent.com/u/49541181?v=4" alt="me")
 </template>
 
@@ -25,6 +25,19 @@ h2
   font-size: 2rem
   font-weight: 200
   margin-bottom: .8rem
-Content
-  width: 100%
+a
+  text-decoration: none
+  font-weight: 600
+  position: relative
+  &:before
+    content: ''
+    background-color: rgba(102, 239, 115, 0.75)
+    width: 100%
+    position: absolute
+    bottom: .5px
+    height: 1px
+    transition: all .35s
+  &:hover
+    &:before
+      background-color: rgb(102, 239, 115)
 </style>
