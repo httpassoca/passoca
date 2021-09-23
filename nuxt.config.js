2a96187117b6070a630051c0049b0512af3a559f
rafonel <trabsom.rafael@gmail.com>
2021-09-23 03:04:02 -0300

    hm

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,8 +27,7 @@ export default {
   postcss: {
     plugins: [
       require('postcss-import'),
-      require('tailwindcss'),
-      require('autoprefixer')
+      require('tailwindcss')
     ]
   },
 
