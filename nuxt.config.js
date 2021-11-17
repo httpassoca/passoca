5a76fd818166ef62eba68feedd0d961be2f66d8e
rafonel <rfreitas@soujmv.com>
2021-11-17 15:42:17 -0300

    hm

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'universal',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -27,7 +27,8 @@ export default {
   postcss: {
     plugins: [
       require('tailwindcss'),
-      require('postcss-import')
+      require('postcss-import'),
+	    require('autoprefixer')
     ]
   },
 
