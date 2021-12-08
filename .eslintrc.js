3ed2d7d5a7c5219f6b3fefa94b4db59b085de180
rafonel <trabsom.rafael@gmail.com>
2021-12-07 23:55:52 -0300

    some package changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
+  parser: '@typescript-eslint/parser', // add the TypeScript parser
   plugins: [
+    'svelte3',
+    '@typescript-eslint' // add the TypeScript plugin
   ],
-  // add your custom rules here
-  rules: {}
-}
+  overrides: [ // this stays the same
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  rules: {
+    // ...
+  },
+  settings: {
+    'svelte3/typescript': () => require('typescript'), // pass the TypeScript package to the Svelte plugin
+    // OR
+    'svelte3/typescript': true, // load TypeScript as peer dependency
+    // ...
+  }
+};
