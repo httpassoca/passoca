b3ed03ab42719e28fc533160edc478b113867e01
rafonel <trabsom.rafael@gmail.com>
2021-12-07 23:56:02 -0300

    beautiful link component

---
+++ src/lib/AppLink.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let to: string;
+  export let blank = true;
+</script>
+
+<a href={to} target={blank && "_blank"} rel="noopener noreferrer">
+  <slot />
+</a>
+
+<style lang="sass">
+a
+  text-decoration: none
+  font-weight: 600
+  position: relative
+  &:before
+    content: ''
+    background-color: #10951c
+    width: 100%
+    position: absolute
+    left: 0
+    bottom: 1px
+    height: 1px
+    transition: all .35s cubic-bezier(0.215,0.61,0.355,1)
+  &:hover
+    &:before
+      box-shadow: 0px -9.6px 18px rgb(102, 239, 115), 0px -26.4px 32.8px rgb(102, 239, 115), 0px -63.6px 65.2px rgb(102, 239, 115), 0px -211px 137px rgb(102, 239, 115)
+      background-color: rgb(102, 239, 115)
+      height: 2px
+</style>
