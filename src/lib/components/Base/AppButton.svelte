9ac2b8467ba51958e35b89a48a992a9bc6b2009b
httpassoca <me@passoca.dev>
2022-04-03 03:12:11 +0100

    fix: change buttons style

--- src/lib/components/Base/AppButton.svelte
+++ src/lib/components/Base/AppButton.svelte
@@ -13,8 +13,7 @@
 <style lang="sass">
 button
   padding: 12px 24px
-  clip-path: polygon(15px 0, 100% 0, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0 100%, 0 15px)
-  @apply my-2 flex items-center gap-2
+  @apply my-2 flex items-center gap-2 rounded
   color: var(--app-color-contrast-text)
   background-color: var(--app-color-primary)
 </style>
--- src/lib/components/Base/AppLink.svelte
+++ src/lib/components/Base/AppLink.svelte
@@ -38,9 +38,8 @@ a
       height: 2px
 
   &.button
+    @apply my-2 flex items-center gap-2 rounded
     padding: 12px 24px
-    clip-path: polygon(15px 0, 100% 0, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0 100%, 0 15px)
-    @apply my-2 flex items-center gap-2
     color: var(--app-color-contrast-text)
     background-color: var(--app-color-primary)
     &:before
