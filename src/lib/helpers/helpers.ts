6b7104b97532351560d7428b5bb605894213a1fb
httpassoca <me@passoca.dev>
2023-04-25 17:44:49 +0100

    feat: add helper to get slug

--- src/lib/helpers/helpers.ts
+++ src/lib/helpers/helpers.ts
@@ -2,3 +2,14 @@ export const capitalize = (string: string) => {
   let capitalized = string.toLowerCase();
   return capitalized.charAt(0).toUpperCase() + capitalized.slice(1);
 }
+
+export const getSlug = (path: string) => {
+  const fileNameRegex = /[^\\/]+$/;
+  const extensionRegex = /\.[^./\\]*$/;
+
+  const base = path.match(fileNameRegex) ? path.match(fileNameRegex)[0] : '';
+  const ext = base.match(extensionRegex) ? base.match(extensionRegex)[0] : '';
+  const slug = base.slice(0, base.length - ext.length);
+
+  return slug;
+}
