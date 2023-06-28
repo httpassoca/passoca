7d5be34f5d5ae9221d13c6eb3516eeea6bb991d3
httpassoca <me@passoca.dev>
2023-06-28 21:52:49 +0100

    add sound

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -13,12 +13,20 @@
   type Options = {
     direction?: "in" | "out" | "both";
   };
+  function playFlush() {
+    const url = "https://cdn.pixabay.com/audio/2021/08/09/audio_e2a6340055.mp3";
+    const audio = new Audio(url);
+    audio.volume = 0.5;
+    audio.play();
+  }
 
   function flush(
     node: Element,
     { delay = 0, duration = 300, easing = cubicIn }: Params = {},
     { direction = "both" }: Options = {}
   ): TransitionConfig {
+    direction === "out" && playFlush();
+
     return {
       delay,
       duration,
