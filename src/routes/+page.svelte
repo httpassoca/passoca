e92d747b7ec72d0e0073c1710c378feb93bdd162
Rafael Freitas <49541181+httpassoca@users.noreply.github.com>
2023-08-30 11:31:20 +0100

    remove about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 <Content page>
   <div class="info">
     <h1 class="font-boston mb-2">Hi, I'm Rafael Passoca.</h1>
-    <p>
+    <p class="text-base">
       As a Frontend Engineer, I believe that appearence and practicity are the
       best influences to the user. <b
         >This is what matters. And I love to make it happen.</b
@@ -48,11 +48,8 @@
         title="resume">click here</a
       >
     </p>
-    <div class="flex mt-4 justify-center">
-      <Link to="/about" button blank={false}>More about me</Link>
-    </div>
   </div>
-  <div class="flex flex-col items-center md:items-start md:flex-row gap-4">
+  <div class="flex flex-col items-center md:items-start md:flex-row gap-4 mt-4">
     <div>
       <div class="image">
         <img
@@ -103,24 +100,24 @@
     font-size: clamp(36px, 8vw, 54px)
   a
     border-bottom: 1px solid var(--app-color-primary)
-  .info
-    a
-      border-bottom: 1px solid var(--app-color-primary)
-  .image
-    position: relative
-    width: 200px
-    height: 200px
-    border-radius: 50%
-    overflow: hidden
-    img
-      position: absolute
-      top: 0
-      left: 0
-      height: 100%
-      width: 100%
-      transition: all .65s cubic-bezier(0.215,0.61,0.355,1)
-      &:hover
-        transform: scale(2)
-        top: 38%
-        left: 10%
+.info
+  a
+    border-bottom: 1px solid var(--app-color-primary)
+.image
+  position: relative
+  width: 200px
+  height: 200px
+  border-radius: 50%
+  overflow: hidden
+  img
+    position: absolute
+    top: 0
+    left: 0
+    height: 100%
+    width: 100%
+    transition: all .65s cubic-bezier(0.215,0.61,0.355,1)
+    &:hover
+      transform: scale(2)
+      top: 38%
+      left: 10%
 </style>
