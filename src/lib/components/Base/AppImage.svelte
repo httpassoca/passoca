d7b55b9058a0be493076db7d9bf6367824588e48
httpassoca <me@passoca.dev>
2024-01-02 13:01:19 +0000

    Add PhotoSwipe for image previews
    Introduced PhotoSwipe to provide a lightbox-style overlay for image
    previews when viewing in the AppImage component. Updated the data
    structure to accommodate the additional image metadata needed for
    PhotoSwipe, such as height and grouping of image resources. Enhanced the
    UI to show a clickable image that triggers the fullscreen preview. Also
    involved adding the necessary styles and functionality imports. This
    feature enhances user experience by offering a better visual
    presentation of images.

--- src/lib/components/Base/AppImage.svelte
+++ src/lib/components/Base/AppImage.svelte
@@ -1,33 +1,62 @@
 <script lang="ts">
-  interface Image {
+  type TImage = {
     url: string;
     width: number;
-  }
+    height: number;
+  };
+  type TImagesRes = {
+    imgs: TImage[];
+    originalImage?: TImage;
+  };
   import { onMount } from "svelte";
   import AppLoader from "./AppLoader.svelte";
+  import "photoswipe/style.css";
+
+  import PhotoSwipeLightbox from "photoswipe/lightbox";
 
   export let post: string;
   export let img: string;
   export let alt: string;
 
-  let images: Image[] = [];
+  let images: TImagesRes = {
+    imgs: [],
+  };
 
   onMount(async () => {
     images = await fetch(
-      `${import.meta.env.VITE_API_URL}/img/${post}/${img}`
+      `${import.meta.env.VITE_API_URL}/img/${post}/${img}`,
     ).then((res) => res.json());
+
+    // Image visualizer
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: `#${alt.split(" ").join("-")}`,
+      children: "a",
+      showHideAnimationType: "zoom",
+      pswpModule: () => import("photoswipe"),
+    });
+    lightbox.init();
   });
 </script>
 
-{#if images.length}
-  <picture>
-    {#each images as image}
-      <source media={`min-width:${image.width}px`} srcset={image.url} />
-    {/each}
-    <img src={images[0].url} {alt} loading="lazy" />
-  </picture>
-{:else}
-  <div class="flex justify-center">
-    <AppLoader height={120} />
-  </div>
-{/if}
+<div class="pswp-gallery" id={alt.split(" ").join("-")}>
+  {images.imgs?.length}
+  {#if images.imgs?.length}
+    <a
+      href={images.originalImage.url}
+      data-pswp-width={images.originalImage.width}
+      data-pswp-height={images.originalImage.height}
+      rel="noreferrer"
+    >
+      <picture>
+        {#each images.imgs as image}
+          <source media={`min-width:${image.width}px`} srcset={image.url} />
+        {/each}
+        <img src={images.imgs[0].url} {alt} loading="lazy" />
+      </picture>
+    </a>
+  {:else}
+    <div class="flex justify-center">
+      <AppLoader height={120} />
+    </div>
+  {/if}
+</div>
