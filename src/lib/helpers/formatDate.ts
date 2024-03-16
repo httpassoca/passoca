9707a6541be568d4fc64e1686793de8cccf35c6f
httpassoca <me@passoca.dev>
2024-03-16 14:28:51 +0000

    feat: posts by year

--- src/lib/helpers/formatDate.ts
+++ src/lib/helpers/formatDate.ts
@@ -2,7 +2,6 @@ export const formatDate = (dateStr: string) => {
   const date = new Date(dateStr);
 
   const dateTimeFormat = new Intl.DateTimeFormat("en", {
-    year: "numeric",
     month: "short",
     day: "2-digit",
   });
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,8 +7,14 @@
 
 <Content page>
   <Title>Posts</Title>
-  <div class="flex flex-col gap-3">
-    {#each posts as post (post.title)}
+  <div class="flex flex-col gap-3 mb-4">
+    {#each posts as post, i (post.title)}
+      {#if i === 0}
+        {post.date.substring(0, 4)}
+      {/if}
+      {#if i && post.date.substring(0, 4) !== posts[i - 1].date.substring(0, 4)}
+        {post.date.substring(0, 4)}
+      {/if}
       <Post {post} />
     {/each}
   </div>
